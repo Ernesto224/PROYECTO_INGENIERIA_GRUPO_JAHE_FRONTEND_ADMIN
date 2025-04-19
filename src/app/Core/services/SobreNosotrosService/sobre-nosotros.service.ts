d7b595955b9f09ac33beb1bacc068edb81d74758
JESNER1680 <129021529+JESNER1680@users.noreply.github.com>
2025-04-19 02:18:54 -0600

    US16 Avance
    Cambiar el texto que se despliega en  la página "Como llegar"

---
+++ src/app/Core/services/SobreNosotrosService/sobre-nosotros.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class SobreNosotrosService {
+  private urlBase: string = '';
+  private http = inject(HttpClient)
+  constructor() { }
+
+  const actualizarDescripcionSobreNosotros: void =() =>{
+    HttpClient.g
+  }
+}
