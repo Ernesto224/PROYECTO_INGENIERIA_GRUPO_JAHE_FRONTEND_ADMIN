9d973a23b4bc932d0f5828d6524a45e184fbf8e8
JESNER1680 <129021529+JESNER1680@users.noreply.github.com>
2025-04-21 02:22:38 -0600

    US-16
    Avance de la pagina Como Llegar

--- src/app/pages/como-llegar/como-llegar.component.ts
+++ src/app/pages/como-llegar/como-llegar.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject,OnInit, effect, signal, computed } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ComoLlegarService } from '../../Core/services/ComoLlegarService/como-llegar.service'; 
+import { DireccionDTO } from '../../Core/models/DireccionDTO';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-como-llegar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './como-llegar.component.html',
   styleUrl: './como-llegar.component.css'
 })
 export class ComoLlegarComponent {
+  private service = inject(ComoLlegarService);
+  readonly direccion = signal<DireccionDTO>({
+    idDireccion: -1,
+    descripcion: ''
+  });
+  readonly confirmacion = signal<boolean>(false);
+  readonly descripcionControl = new FormControl('');
+  ngOnInit(){
+    this.obtenerInformacion();
+  }
 
+  obtenerInformacion(): void{
+    this.service.obtenerDatosComoLlegar().subscribe({
+     next: (data)=>{
+      this.direccion.set(data);
+      this.descripcionControl.setValue(data.descripcion);
+     },
+     error: (Error)=>{
+      console.error("Hubo un error", Error);
+     },
+     complete: ()=>{
+      console.log("Se completó la operación");
+     }
+    })
+  }
+  actualizarInformacion(): void{
+    const descripcionActualizada = this.descripcionControl.value??'';
+    const DireccionActualizada = {...this.direccion(), descripcion: descripcionActualizada} 
+    this.service.enviarNuevoTextoComoLlegar(DireccionActualizada).subscribe({
+      next: (data)=>{
+        this.direccion.set(data);
+      },
+      error: (Error)=>{
+        console.error("Hubo un error al actualizar", Error);
+      },
+      complete: ()=>{
+        console.log("Actualizacion completada");
+      }
+    });
+  }
 }
