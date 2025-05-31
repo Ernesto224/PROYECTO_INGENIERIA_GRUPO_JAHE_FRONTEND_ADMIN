6bed2a88a859feba3b55d3177c86f9684d528248
Ernesto224 <ev402648@gmail.com>
2025-05-31 06:30:26 -0600

    Ultimas modificaciones a US25

--- src/app/Core/components/header/header.component.ts
+++ src/app/Core/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SeguridadService } from '../../services/SeguridadService/seguridad.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
